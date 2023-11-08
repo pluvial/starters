53f7812ae3aee57055d1acacb09f59cc64d7205f
jpaquim <joaopaquim@gmail.com>
2023-11-08 11:20:25 +0000

    Create vite templates in parallel

--- create-vite.js
+++ create-vite.js
@@ -27,8 +27,25 @@ const templates = [
   'qwik-ts',
 ];
 
-for (const template of templates) {
-  await $`pnpm create vite --template ${template} ${template}`;
+// non-parallel version
+// for (const template of templates) {
+//   await $`pnpm create vite --template ${template} ${template}`;
+// }
+
+const results = await Promise.allSettled(
+  templates.map(template => $`pnpm create vite --template ${template} ${template}`.timeout('10s')),
+);
+
+const failed = results.map((result, i) => (result.status === 'rejected' ? i : null)).filter(x => x);
+if (failed.length > 0) {
+  console.warn(`Failed templates: ${failed.map(i => templates[i])}`);
+  // const reasons = failed.map(i => results[i].reason);
+  // for (const stdout of reasons.map(reason => reason.stdout)) {
+  //   console.warn(stdout);
+  // }
+  // for (const stderr of reasons.map(reason => reason.stderr)) {
+  //   console.error(stderr);
+  // }
 }
 
 cd('..');
