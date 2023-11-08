12ab5dc17080b64669c3f763c6dbcc40ddf2e3b4
jpaquim <joaopaquim@gmail.com>
2023-11-08 16:41:01 +0000

    Create qwik templates in parallel

--- create-qwik.js
+++ create-qwik.js
@@ -8,14 +8,22 @@ cd('qwik');
 
 const templates = ['basic', 'empty', 'site-with-visual-cms', 'library'];
 
-const failed = [];
-for (const template of templates) {
-  try {
-    await $`pnpm create qwik ${template} ${template}`;
-  } catch {
-    failed.push(template);
-  }
-}
+// non-parallel version
+// const failed = [];
+// for (const template of templates) {
+//   try {
+//     await $`pnpm create qwik ${template} ${template}`;
+//   } catch {
+//     failed.push(template);
+//   }
+// }
+
+const results = await Promise.allSettled(
+  templates.map(template => $`pnpm create qwik ${template} ${template}`),
+);
+const failed = results
+  .map((result, i) => (result.status === 'rejected' ? templates[i] : null))
+  .filter(x => x);
 
 if (failed.length > 0) {
   console.warn(`Failed templates: ${failed}`);
