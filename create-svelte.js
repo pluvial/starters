d29940c84e9971e9f1aec8f04ec8f7162ff6d102
jpaquim <joaopaquim@gmail.com>
2023-11-08 12:17:43 +0000

    Create permutations of svelte templates

--- create-svelte.js
+++ create-svelte.js
@@ -2,18 +2,46 @@
 import { create } from 'create-svelte';
 import 'zx/globals';
 
+/** @typedef {Parameters<typeof create>[1]} Options  */
+
 await $`rm -rf svelte`;
 await $`mkdir -p svelte`;
 cd('svelte');
 
-await create('default', {
-  name: 'default',
-  template: 'default', // or 'skeleton' or 'skeletonlib'
-  types: 'checkjs', // or 'typescript' or null
-  prettier: false,
-  eslint: false,
-  playwright: false,
-  vitest: false,
-});
+const bool = [false, true];
+const templates = ['default', 'skeleton', 'skeletonlib'];
+const types = [null, 'checkjs', 'typescript'];
+
+const permutations = [templates, types, bool, bool, bool, bool].reduce(
+  (acc, arr) => acc.flatMap(el => arr.map(val => [...el, val])),
+  [[]],
+);
+
+for (const [template, types, prettier, eslint, playwright, vitest] of permutations) {
+  const path = [
+    template,
+    types && `-${types}`,
+    prettier && '-prettier',
+    eslint && '-eslint',
+    playwright && '-playwright',
+    vitest && '-vitest',
+  ]
+    .filter(Boolean)
+    .join('');
+  const name = `svelte-${path}`;
+
+  /** @type {Options} */
+  const options = {
+    name,
+    template,
+    types,
+    prettier,
+    eslint,
+    playwright,
+    vitest,
+  };
+  console.log(JSON.stringify({ path, options }));
+  await create(path, options);
+}
 
 cd('..');
