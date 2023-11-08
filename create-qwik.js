037e365fa897e4e55d7877922ecf51355895e3b2
jpaquim <joaopaquim@gmail.com>
2023-11-08 16:39:18 +0000

    Add rest of qwik templates

--- create-qwik.js
+++ create-qwik.js
@@ -5,5 +5,20 @@ import 'zx/globals';
 await $`rm -rf qwik`;
 await $`mkdir -p qwik`;
 cd('qwik');
-await $`pnpm create qwik basic basic`;
+
+const templates = ['basic', 'empty', 'site-with-visual-cms', 'library'];
+
+const failed = [];
+for (const template of templates) {
+  try {
+    await $`pnpm create qwik ${template} ${template}`;
+  } catch {
+    failed.push(template);
+  }
+}
+
+if (failed.length > 0) {
+  console.warn(`Failed templates: ${failed}`);
+}
+
 cd('..');
