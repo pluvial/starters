1567349cb7f3f401bd6302083d54837757c52d66
jpaquim <joaopaquim@gmail.com>
2023-11-08 17:56:13 +0000

    Add rest of solid templates (broken)

--- create-solid.js
+++ create-solid.js
@@ -5,6 +5,36 @@ import 'zx/globals';
 await $`rm -rf solid`;
 await $`mkdir -p solid`;
 cd('solid');
-const answers = $`echo bare; sleep 1; echo yes; sleep 1; echo yes; sleep 1; echo yes`;
-await answers.pipe($`pnpm create solid bare`);
+
+const templates = [
+  'bare',
+  'hackernews',
+  'movies',
+  'notes',
+  'todomvc',
+  'with-auth',
+  'with-authjs',
+  'with-mdx',
+  'with-prisma',
+  'with-solid-styled',
+  'with-tailwindcss',
+  'with-trpc',
+  'with-vitest',
+  'with-websocket',
+];
+
+const failed = [];
+for (const template of templates) {
+  try {
+    const answers = $`echo ${template}; sleep 1; echo yes; sleep 1; echo yes; sleep 1; echo yes`;
+    await answers.pipe($`pnpm create solid ${template}`);
+  } catch {
+    failed.push(template);
+  }
+}
+
+if (failed.length > 0) {
+  console.warn(`Failed templates: ${failed}`);
+}
+
 cd('..');
