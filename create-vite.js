805b3107df617b0c4f031f13b8473935985130dc
jpaquim <joaopaquim@gmail.com>
2023-11-08 10:44:50 +0000

    Add rest of vite templates

--- create-vite.js
+++ create-vite.js
@@ -5,5 +5,30 @@ import 'zx/globals';
 await $`rm -rf vite`;
 await $`mkdir -p vite`;
 cd('vite');
-await $`pnpm create vite --template vanilla vanilla`;
+
+const templates = [
+  'vanilla',
+  'vanilla-ts',
+  'vue',
+  'vue-ts',
+  'react',
+  'react-ts',
+  'react-swc',
+  'react-swc-ts',
+  'preact',
+  'preact-ts',
+  'lit',
+  'lit-ts',
+  'svelte',
+  'svelte-ts',
+  'solid',
+  'solid-ts',
+  'qwik',
+  'qwik-ts',
+];
+
+for (const template of templates) {
+  await $`pnpm create vite --template ${template} ${template}`;
+}
+
 cd('..');
