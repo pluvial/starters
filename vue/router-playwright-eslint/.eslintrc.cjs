f36f4714ca80209a4e99f999076b44f186d2338b
jpaquim <joaopaquim@gmail.com>
2024-08-08 16:15:35 +0000

    Update vue templates to latest versions

--- vue/router-playwright-eslint/.eslintrc.cjs
+++ vue/router-playwright-eslint/.eslintrc.cjs
@@ -5,6 +5,16 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
+  overrides: [
+    {
+      files: [
+        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
+      ],
+      'extends': [
+        'plugin:playwright/recommended'
+      ]
+    }
+  ],
   parserOptions: {
     ecmaVersion: 'latest'
   }
